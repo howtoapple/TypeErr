e7d05faddf227c8a865086109fb3b8e060cd502d
howtoapple <74847942+howtoapple@users.noreply.github.com>
2022-05-23 22:01:22 -0600

    Redo

--- main/script.js
+++ main/script.js
@@ -1,18 +1,4 @@
-function drop() {
-  document.getElementById("authorDropdown").classList.toggle("show");
-}
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-      }
-    } 
-  }
+
   function sites() {
     document.getElementById("siteDropdown").classList.toggle("show");
   }
